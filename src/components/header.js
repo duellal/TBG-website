3a6b56faa25fc334f93b6d3ed142e69886d68e68
Alex <alexandriaduell@gmail.com>
2023-12-05 15:31:29 -0500

    started on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,48 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import {
+    Nav, 
+    NavItem, 
+    Navbar } from "reactstrap";
 
-// //About Styles:
-// import "../styles/css/about.css"
+//Logo
+import logo from "../images/logo192.png"
+
+// //Header Styles:
+// import "../styles/css/header.css"
 
 export default function Header(){
 
+    return (
+        <header>
+            <div className="logo">
+                <img className="logo-png" alt="The Biscuit Garden Logo - White dog with a bowtie encircled by the words, the biscuit garden" src={logo}/>
+            </div>
+            <Navbar>
+                <Nav>
+                    <NavItem className="home">
+                            Home
+                    </NavItem>
+                    <NavItem className="about">
+                            About
+                    </NavItem>
+                    <NavItem className="boarding">
+                            Boarding
+                    </NavItem>
+                    <NavItem className="daycare">
+                            Daycare
+                    </NavItem>
+                    <NavItem className="grooming">
+                            Grooming
+                    </NavItem>
+                    <NavItem className="forms">
+                            Forms
+                    </NavItem>
+                    <NavItem className="faq">
+                            FAQs
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </header>
+    )
 }
