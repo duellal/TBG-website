209249991c5fab8a987d1409171de5784259514f
Alex <alexandriaduell@gmail.com>
2023-12-10 21:02:08 -0500

    Cont working on footer

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -17,80 +17,103 @@ export default function Footer(){
 
     return (
         <footer>
-            <div className="hours">
-                <p>
-                    Monday - Saturday
-                </p>
-                <p>
-                    7am - 7pm
-                </p>
-                <p>
-                    Sunday
-                </p>
-                <p>
-                    4pm - 7pm
-                </p>
-            </div>  
+            <div id="info">
+                <div id="first-col">
+                    <div className="hours">
+                        <p className="title">
+                            Hours
+                        </p>
+                        <p>
+                            Monday - Saturday
+                        </p>
+                        <p>
+                            7am - 7pm
+                        </p>
+                        <p>
+                            Sunday
+                        </p>
+                        <p>
+                            4pm - 7pm
+                        </p>
+                    </div>  
 
-            <div className="holiday-hours">
-                <p>
-                    Closed
-                </p>
-                <p>
-                    New Year's Day, Easter Day, Independence Day, Thanksgiving Day, Christmas Day
-                </p>
-            </div>  
+                    <div className="holiday-hours">
+                        <p className="title">
+                            Holiday Hours
+                        </p>
+                        <p>
+                            Closed
+                        </p>
+                        <p>
+                            New Year's Day, Easter Day, Independence Day, Thanksgiving Day, Christmas Day
+                        </p>
+                    </div> 
+                </div>
 
-            <div className="contact">
-                <p className="title">
-                    Contact Us!
-                </p>
-                <p className="address">
-                    2112 E Williams St, Apex, NC 27539
-                </p>
-                <p className="phone">
-                    (919) 355 - 2820
-                </p>
-                <p className="email">
-                    thebiscuitgarden@gmail.com
-                </p>
-                <p className="facebook">
-                    /thebiscuitgarden
-                </p>
-            </div>
-             <Navbar>
-                <Nav tabs fill>
-                    <NavItem onClick={() => navigate('/about/contact')}>
-                            Contact Us
-                    </NavItem>
-                    <NavItem onClick={() => navigate('/boarding')}>
-                            Boarding
-                    </NavItem>
-                    <NavItem onClick={() => navigate('/daycare')}>
-                            Daycare
-                    </NavItem>
-                    <NavItem onClick={() => navigate('/grooming')}>
-                            Grooming
-                    </NavItem>
-                    <NavItem onClick={() => navigate('/forms')}>
-                            Forms
-                    </NavItem>
-                    <NavItem onClick={() => navigate('/faqs')}>
-                            FAQs
-                    </NavItem>
-                    <NavItem onClick={() => navigate('/sitemap')}>
-                            Sitemap
-                    </NavItem>
-                </Nav>
-            </Navbar>
+                <div id="second-col">
+                    <div className="contact">
+                        <p className="title">
+                            Contact Us!
+                        </p>
+                        <a className='address' href="https://maps.app.goo.gl/xP4CiNAGKqzPhW138" target="_blank" rel="noreferrer">
+                            2112 E Williams St, Apex, NC 27539
+                        </a>
+                        <p className="phone">
+                            (919) 355 - 2820
+                        </p>
+                        <a className="email" href="mailto:thebiscuitgarden@gmail.com">
+                            thebiscuitgarden@gmail.com
+                        </a>
+                        <a className="facebook" href="https://www.facebook.com/Apex2112EWilliamsSt" target="_blank" rel="noreferrer">
+                            /thebiscuitgarden
+                        </a>
+                    </div>
+                </div>
+                
+                <div id="third-col">
+                    <Navbar>
+                        <p>
+                            Quick Links
+                        </p>
+                        <Nav tabs fill>
+                            <NavItem onClick={() => navigate('/about/contact')}>
+                                    Contact Us
+                            </NavItem>
+                            <NavItem onClick={() => navigate('/boarding')}>
+                                    Boarding
+                            </NavItem>
+                            <NavItem onClick={() => navigate('/daycare')}>
+                                    Daycare
+                            </NavItem>
+                            <NavItem onClick={() => navigate('/grooming')}>
+                                    Grooming
+                            </NavItem>
+                            <NavItem onClick={() => navigate('/forms')}>
+                                    Forms
+                            </NavItem>
+                            <NavItem onClick={() => navigate('/faqs')}>
+                                    FAQs
+                            </NavItem>
+                            <NavItem onClick={() => navigate('/sitemap')}>
+                                    Sitemap
+                            </NavItem>
+                        </Nav>
+                    </Navbar>
 
-            <div className="copyright">
-                <p className="name">
-                    The Biscuit Garden
-                </p>
-                <p className="year">
-                    2024
-                </p>
+                    <div className="logo">
+                        <img className="logo-png" alt="The Biscuit Garden Logo - White dog with a bowtie encircled by the words, the biscuit garden" src={logo}/>
+                    </div>
+                </div>
+
+
+                <div className="copyright">
+                    <p className="name">
+                        The Biscuit Garden
+                    </p>
+                    <p className="year">
+                        2024
+                    </p>
+                </div>
             </div>
         </footer>
     )
