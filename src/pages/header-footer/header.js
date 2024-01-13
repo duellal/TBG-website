a25cb8ea46171bd552e461580fada701ede86ee7
Alexandria Duell <72361295+duellal@users.noreply.github.com>
2024-01-12 23:57:38 -0500

    Revert "Responsive Header"

--- src/pages/header-footer/header.js
+++ src/pages/header-footer/header.js
@@ -1,100 +1,151 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import React from "react";
+import {
+    Nav, 
+    NavItem, 
+    Navbar } from "reactstrap";
 import { useNavigate } from "react-router";
 import { useLocation } from "react-router-dom";
-import { Squash as Hamburger } from 'hamburger-react'
 
 //Logo
 import logo from "../../images/logo192.png";
 
 //Header Styles:
-// import "../../styles/header.css";
-import { LogoContainer, Logo, HamburgerDiv, MobileNav, MobileNavBar, Nav, Navbar, StyledHeader } from '../../styles/header'
-
-//Components
-import NavItem from "./NavItem";
+import "../../styles/header.css";
 
 //Options for dropdown menu:
 import {
-    options
+    aboutOptions,
+    boardingOptions,
+    daycareOptions,
+    groomingOptions, 
+    faqOptions
 } from './menuOptions'
 
 
-export default function Header() {
-    const [isOpen, setOpen] = useState(false)
+export default function Header(){
     let navigate = useNavigate();
     let location = useLocation();
     let path = location.pathname;
 
     const scrollToElem = (section) => {
         let elem = document.getElementById(section)
-        elem.scrollIntoView({ behavior: 'smooth' })
+        elem.scrollIntoView({behavior: 'smooth'})
     }
 
     let url = async (urlOption) => {
         let splitUrlHash = urlOption.split(`#`)
 
-        if (urlOption === `home`) {
-            if (path === '/') {
-                scrollToElem(urlOption)
+        if(urlOption === `home`){
+            if(path === '/'){
+                return scrollToElem(urlOption)
             }
-            else {
-                navigate('/')
+            else{
+                return navigate('/')
             }
         }
-        else if (urlOption.includes(`#`)) {
+        else if(urlOption.includes(`#`)){
             let id = splitUrlHash[1];
             await navigate(urlOption)
-            scrollToElem(id)
+            return scrollToElem(id)
         }
-        else if (urlOption.includes('team')) {
+        else if(urlOption.includes('team')){
             let teamId = `teamCareers`
             let prevLocation = window.location.href
 
-            if (prevLocation.includes(`#`)) {
+            if(prevLocation.includes(`#`)){
                 await navigate(urlOption)
-                scrollToElem(teamId)
+                return scrollToElem(teamId)
             }
-            else {
+            else{
                 navigate(`about/team`)
-            }
+            }   
         }
-        else if (path === urlOption) {
+        else if(path === urlOption){
             let elem = urlOption.split(`/`)[1]
             let getId = document.getElementById(elem)
-            getId.scrollIntoView({ behavior: 'smooth' })
+            return getId.scrollIntoView({behavior: 'smooth'})
         }
-        else {
+        else{
             navigate(urlOption)
-        }
-
-        setOpen(false)
+        } 
     }
 
     return (
-        <StyledHeader>
-            <LogoContainer className="logo">
-                <Logo loading="lazy" className="logo-png" alt="The Biscuit Garden Logo - White dog with a bowtie encircled by the words, the biscuit garden" src={logo} />
-            </LogoContainer>
+        <header>
+            <div className="logo">
+                <img loading="lazy" className="logo-png" alt="The Biscuit Garden Logo - White dog with a bowtie encircled by the words, the biscuit garden" src={logo}/>
+            </div>
             <Navbar>
                 <Nav>
-                    {Object.keys(options).map(key => {
-                        return <NavItem options={options[key]} url={url} title={key} />
-                    })}
+                    <NavItem className="home" onClick={() => url(`home`)}>
+                            Home
+                    </NavItem>
+                    <NavItem className="about">
+                        <button className="dropbtn"  onClick={() => url(`/about`)}>
+                            About
+                        </button>
+                        <div className="dropdown-content">
+                            {aboutOptions.map((option, i) => {
+                                return (
+                                    <button onClick={() => url(option.url)} key={i}> {option.title} </button>
+                                )
+                            })}
+                        </div>
+                    </NavItem>
+                    <NavItem className="boarding">
+                        <button className="dropbtn" onClick={() => url(`/boarding`)}>
+                            Boarding
+                        </button>
+                        <div className="dropdown-content">
+                            {boardingOptions.map((option, i) => {
+                                return (
+                                    <button onClick={() => url(option.url)} key={i}> {option.title} </button>
+                                )
+                            })} 
+                        </div>
+                    </NavItem>
+                    <NavItem className="daycare">
+                        <button className="dropbtn" onClick={() => url(`/daycare`)}>
+                            Daycare
+                        </button>
+                        <div className="dropdown-content">
+                            {daycareOptions.map((option, i) => {
+                                return (
+                                    <button onClick={() => url(option.url)} key={i}> {option.title} </button>
+                                )
+                            })} 
+                        </div>
+                    </NavItem>
+                    <NavItem className="grooming">
+                        <button className="dropbtn"  onClick={() => url(`/grooming`)}>
+                            Grooming
+                        </button>
+                        <div className="dropdown-content">
+                            {groomingOptions.map((option, i) => {
+                                return (
+                                    <button onClick={() => url(option.url)} key={i}> {option.title} </button>
+                                )
+                            })}
+                        </div>
+                    </NavItem>
+                    <NavItem className="forms" onClick={() => url(`/forms`)}>
+                        Forms
+                    </NavItem>
+                    <NavItem className="faq">
+                        <button className="dropbtn" onClick={() => url(`/faqs`)}>
+                            FAQs
+                        </button>
+                        <div className="dropdown-content">
+                            {faqOptions.map((option, i) => {
+                                return (
+                                    <button onClick={() => url(option.url)} key={i}> {option.title} </button>
+                                )
+                            })}
+                        </div>
+                    </NavItem>
                 </Nav>
             </Navbar>
-            <HamburgerDiv>
-                <Hamburger toggled={isOpen} toggle={setOpen} color="white" />
-            </HamburgerDiv>
-            {isOpen &&
-                <MobileNavBar>
-                    <MobileNav>
-                        {Object.keys(options).map(key => {
-                            return <NavItem options={options[key]} url={url} title={key} isMobile={true} />
-                        })}
-                    </MobileNav>
-                </MobileNavBar>
-            }
-        </StyledHeader>
+        </header>
     )
 }
