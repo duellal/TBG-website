7c568424790a896da85bc16b3743ee0116bcd451
Alex <alexandriaduell@gmail.com>
2024-01-23 22:04:59 -0500

    Took out check in/out + put in requirements

--- src/pages/daycare.js
+++ src/pages/daycare.js
@@ -12,6 +12,7 @@ import React, { useState } from "react";
 
 //Components:
 import Banner from "./general-components/banner-pic";
+import Requirements from "./general-components/requirements";
 
 //Daycare Styles:
 import '../styles/daycare.css';
@@ -30,8 +31,6 @@ import { daycareValues } from "../constants/values";
 import { daycareImages } from "../constants/banner-pics";
 
 export default function Daycare(){
-    const [toggle, setToggle] = useState(true);
-
     let listValues = daycareValues.map((statement) => {
         return <li>{statement}</li>
     })
@@ -92,121 +91,9 @@ export default function Daycare(){
                         </div>
                         <img src={littlesDaycare1} alt='' className="pic right"/>
                     </div>
-
-                    <div id="bottom">
-                        <img src={littlesDaycare2} alt='' className="pic left"/>
-                        <div className="text">
-                            <h3>Daycare Packages</h3>
-                            <div className="left">
-                                <h4>10 Day Package</h4>
-                                <br/>
-                                <p>Price Here</p>
-                            </div>
-                            <div className="right">
-                                <h4>20 Day Package</h4>
-                                <br/>
-                                <p>Price Here</p>
-                            </div>
-                        </div>
-                        <img src={bigsDaycare2} alt='' className="pic right"/>
-                    </div>
                 </div>
 
-                <div id="checkin-out">
-                    <h2> Daycare Checking In and Out</h2>
-                    <div className="info">
-                        <p> General information about checking in and out at our facility. </p>
-                    </div>
-                    <div className="toggle-box">
-                        <div className="buttons">
-                            <button className={`checkin ${toggle ? 'btn-toggle' : ''}`} onClick={() => {
-                                if(toggle === false){
-                                    setToggle(!toggle)
-                                }
-                            }}>
-                                Check In
-                            </button>
-                            <button className={`checkout ${!toggle ? 'btn-toggle' : ''}`} onClick={() => {
-                                if(toggle === true){
-                                    setToggle(!toggle)
-                                }
-                            }}>
-                                Check Out
-                            </button>
-                        </div>
-
-                        {/* Toggling between the two buttons for check in / out */}
-                        {toggle ?   
-                        <div id="checkin" className="toggle">
-                            <div id="times">
-                                <h3> Typical Times to Check In</h3>
-                                <div className="days">
-                                    <div className="full">
-                                        <h4>Full Days</h4>
-                                        <h5> Monday - Saturday </h5>
-                                        <p>7:15am - 11am </p>
-                                    </div>
-
-                                    <div className="half">
-                                        <h4>Half Days</h4>
-                                        <h5> Monday - Saturday </h5>
-                                        <div className="am-pm">
-                                            <p className="am">AM:</p>
-                                            <p>7:15am - 9am</p>
-                                            <p className="pm">PM:</p>
-                                            <p>2pm - 4pm</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div id="checklist">
-                                <h3>What to Expect</h3>
-                                <div className="text">
-                                    <p className="first">Paragraph on what to expect when checking in.</p>
-                                    <p>Check list of what the customer needs to do before their pet visits us:
-                                        <div className="list">
-                                            <li>One</li>
-                                            <li>Two</li>
-                                            <li>Three</li>
-                                        </div>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        :
-                        <div id="checkout" className="toggle">
-                            <div id="times">
-                                <h3> Typical Times to Check Out</h3>
-                                <div className="days">
-                                    <div className="full">
-                                        <h4>Full Days</h4>
-                                        <h5> Monday - Saturday </h5>
-                                        <p>8am - 6pm</p>
-                                    </div>
-
-                                    <div className="half">
-                                        <h4>Half Days</h4>
-                                        <h5> Monday - Saturday </h5>
-                                            <div className="am-pm">
-                                                <p className="am">AM:</p>
-                                                <p>7:15am - 9am</p>
-                                                <p className="pm">PM:</p>
-                                                <p>2pm - 4pm</p>
-                                            </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div id="checklist">                      
-                                <h3>What to Expect</h3>
-                                <div className="text">
-                                    <p>Paragraph on what to expect when checking out.</p>
-                                </div>
-                            </div>
-                        </div>
-                        }
-                    </div>
-                </div>
+                <Requirements/>
             </div>
         </>
     )
