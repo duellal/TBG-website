5b2ee8b06d578d4cda2d95530b26646973a51ed8
Alex <alexandriaduell@gmail.com>
2024-01-24 00:54:39 -0500

    About.js has styled components + teams/careers component added to the page

---
+++ src/styles/about.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const AboutImg = styled.img`
+width: 100%;
+height: 400px;
+margin-top: 5%;
+`
+
+export const AboutImgDiv = styled.div`
+width: 100%;
+`
+
