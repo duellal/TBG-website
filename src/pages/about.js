ba400b59af628e3625f38718ea3cd79a866e56ed
Alex <alexandriaduell@gmail.com>
2024-01-24 00:41:40 -0500

    Started styled components in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,8 @@ import React from "react";
 //About Styles:
 import "../styles/about.css"
 import '../styles/team_careers.css'
+import { CommonH1, CommonStartDiv, CommonInfoSection, CommonH2 } from "../styles/commonBDG";
+import { teamImg } from "../styles/about.js";
 
 //Images:
 import stockFacility from '../images/facility/facility-img.jpeg'
@@ -13,11 +15,11 @@ import team from '../images/about/team.jpeg';
 
 export default function About(){
     return (
-        <div id="about">
-            <h1>
+        <CommonStartDiv>
+            <CommonH1>
                 A Little About Us
-            </h1>
-            <div className="info-div">
+            </CommonH1>
+            <CommonInfoSection>
                 <p className="p1-info">
                     Bag stretch behind the couch  claw drapes destroy couch chase imaginary bugs  intrigued by the shower give attitude hide when guests come over hopped up on goofballs, flop over leave dead animals as gifts chew ipad power cord  shake treat  hunt anything that moves  intently sniff hand  make muffins, need to chase tail  rub face on everything  sweet beast under the bed attack feet flop over need to chase tail. Give attitude chew ipad power cord  flop over  rub face on everything hopped up on goofballs sweet beast under the bed  intrigued by the shower chase imaginary bugs make muffins  bag stretch,  claw drapes attack feet hunt anything that moves  intently sniff hand  hide when guests come over flop over destroy couch behind the couch. Make muffins chew ipad power cord  sweet beast under the bed attack feet flop over chase imaginary bugs shake treat  bag stretch, hopped up on goofballs  intrigued by the shower need to chase tail destroy couch behind the couch hunt anything that moves  intently sniff hand ,  rub face on everything  hide when guests come over  claw drapes give attitude flop over.
                 </p>
@@ -25,16 +27,16 @@ export default function About(){
                 <p className="p2-info">
                     Hide when guests come over bag stretch hopped up on goofballs make muffins sweet beast under the bed  claw drapes hunt anything that moves flop over need to chase tail, flop over attack feet  rub face on everything leave dead animals as gifts  chase imaginary bugs chew ipad power cord  give attitude destroy couch,  intrigued by the shower shake treat  behind the couch  intently sniff hand  shake treat  flop over  rub face on everything. Make muffins destroy couch  intently sniff hand   rub face on everything bag stretch give attitude behind the couch sweet beast under the bed attack feet flop over shake treat , flop over hunt anything that moves  claw drapes leave dead animals as gifts  intrigued by the shower need to chase tail hopped up on goofballs chase imaginary bugs , hide when guests come over chew ipad power cord  sweet beast under the bed flop over  intrigued by the shower make muffins shake treat  destroy couch chew ipad power cord . Flop over  claw drapes flop over leave dead animals as gifts chew ipad power cord   sweet beast under the bed behind the couch, shake treat  hunt anything that moves hide when guests come over  rub face on everything destroy couch chase imaginary bugs  intently sniff hand ,  intrigued by the shower make muffins attack feet need to chase tail bag stretch hopped up on goofballs. Hopped up on goofballs bag stretch destroy couch attack feet give attitude flop over chew ipad power cord  make muffins need to chase tail flop over,  rub face on everything behind the couch  claw drapes hunt anything that moves chase imaginary bugs  intently sniff hand.
                 </p>
-            </div>
+            </CommonInfoSection>
 
             <div className="image">
                 <img loading="lazy" src={stockFacility} alt='A black dog with tongue sticking out and wide eyes on a persons lap'></img>
             </div>
 
-            <div id="facility">
-                <h2>
+            <CommonInfoSection>
+                <CommonH2>
                     Our Facility 
-                </h2>
+                </CommonH2>
                 <p className="p1-facility">
                     Rub face on everything inspect anything brought into the house hunt anything that moves hopped up on goofballs bag stretch destroy couch lick butt  intrigued by the shower, behind the couch attack feet give attitude  intently stare at the same spot leave dead animals as gifts  claw drapes flop over, stretch lick butt sweet beast under the bed hide when guests come over  rub face on everything leave dead animals as gifts  claw drapes flop over  stand in front of the computer screen  flop over inspect anything brought into the house.  destroy couch leave dead animals as gifts lick butt  rub face on everything give attitude hopped up on goofballs inspect anything brought into the house intently stare at the same spot,  stand in front of the computer screen  hide when guests come over make muffins bag stretch flop over  claw drapes why must they do that attack feet, behind the couch all of a sudden go crazy  intrigued by the shower sweet beast under the bed hunt anything that moves give attitude  stand in front of the computer screen . 
                 </p>
@@ -42,81 +44,81 @@ export default function About(){
                 <p className="p2-facility">
                 Attack feet behind the couch destroy couch flop over give attitude hide when guests come over hopped up on goofballs, hunt anything that moves sweet beast under the bed  stand in front of the computer screen  all of a sudden go crazy intently stare at the same spot leave dead animals as gifts why must they do that, bag stretch lick butt  claw drapes  intrigued by the shower  rub face on everything.  rub face on everything  claw drapes make muffins intently stare at the same spot hunt anything that moves bag stretch all of a sudden go crazy give attitude why must they do that,  destroy couch inspect anything brought into the house sweet beast under the bed flop over hopped up on goofballs  stand in front of the computer screen , hide when guests come over leave dead animals as gifts attack feet  intrigued by the shower behind the couch lick butt inspect anything brought into the house.
                 </p>
-            </div>
+            </CommonInfoSection>
 
-            <div id='teamCareers'>
-                <h1> 
+            <CommonInfoSection>
+                <CommonH1> 
                     Our Team
-                </h1>
+                </CommonH1>
 
-                <div className="info-div">
-                        <h2>
-                            Meet the Owner!
-                        </h2>
+                <CommonInfoSection>
+                    <CommonH2>
+                        Meet the Owner!
+                    </CommonH2>
 
-                        <div className="bio">
-                            <div className="pic left">
-                                <img loading="lazy" src={owner} width='100%' alt="a white short-haired cat with a black face and blue eyes close up" />
-                            </div>
-                            <div className="text right">
-                                <p>
-                                    Puppy kitty ipsum dolor sit good dog throw wet nose paws barky dragging tuxedo running nest cockatiel tongue. Harness slobbery birds pet supplies dragging paws cockatiel bird commands treats scratch left paw food chew stay grooming crate cage park paws. Water leash critters cage gimme five slobber. Bird Food kisses run fast wet nose purr slobbery run cage chew bedding food scratch leash dog house dinnertime roll over slobbery right paw. Cage Fido yawn chow swimming Rover bark Scooby snacks house train hamster. Food small animals slobbery smooshy running sit. Throw bedding parrot Scooby snacks biscuit bark Spike furry.
-                                </p>
-                            </div>
+                    <div className="bio">
+                        <div className="pic left">
+                        <teamImg loading="lazy" src={owner} alt="a white short-haired cat with a black face and blue eyes close up" />
                         </div>
-                </div>
-
-                <div className="info-div">
-                        <h2>
-                            Meet the Manager!
-                        </h2>
-
-                        <div className="bio">
-                            <div className="text left">
-                                <p>
-                                    Raising or, cheep in plows. In the straw rain barrels. Apples ducks straw, quail a ostriches donkey, hay hook cucumbers. Prairie dogs raccoons robins rats. Blue berries pigeons buzz and bean prairie dogs nails at est. Ewes mushrooms zucchini in forage Harvester at sheep with tractor. Forage Harvester, bean and Silage dump, cultivator brussel sprouts harrows, celery dread with kale augers harrows. bull bowels cat chicken cow, calf donkey duck. Veterinarian at Seeder eggs with watermelon ostriches. Forag.
-                                </p>
-                            </div>
-
-                            <div className="pic right">
-                                <img loading="lazy" src={manager} width='100%' alt="dark brown curly haired woman snuggling a white dog" />
-                            </div>
+                        <div className="text right">
+                            <p>
+                                Puppy kitty ipsum dolor sit good dog throw wet nose paws barky dragging tuxedo running nest cockatiel tongue. Harness slobbery birds pet supplies dragging paws cockatiel bird commands treats scratch left paw food chew stay grooming crate cage park paws. Water leash critters cage gimme five slobber. Bird Food kisses run fast wet nose purr slobbery run cage chew bedding food scratch leash dog house dinnertime roll over slobbery right paw. Cage Fido yawn chow swimming Rover bark Scooby snacks house train hamster. Food small animals slobbery smooshy running sit. Throw bedding parrot Scooby snacks biscuit bark Spike furry.
+                            </p>
                         </div>
-                </div>
+                    </div>
+                </CommonInfoSection>
 
-                <div className="info-div">
-                        <h2>
-                            Meet the Team!
-                        </h2>
+                <CommonInfoSection>
+                    <CommonH2>
+                        Meet the Manager!
+                    </CommonH2>
 
-                        <div className="bio">
-                            <div className="pic left">
-                                <img loading="lazy" src={team} width='100%' alt="four dogs tugging at a toy" />
-                            </div>
-                            <div className="text right">
-                                <p>
-                                    Veterinarian at Seeder eggs with watermelon ostriches. Pick up truck livestock, pets and storage shed, troughs feed bale manure, is garden wheat oats at augers. Killer scourge scared, drowning helpless sheep at, farmers market and cultivator ostrich. John Deere bees, parsley sweet corn at, porky pig shovels. Kidney beans ostrich trucks. Lamb in eggplant baler rain barrels manure hay rake. Killer scourge scared, drowning helpless sheep at.
-                                </p>
-                            </div>
+                    <div className="bio">
+                        <div className="text left">
+                            <p>
+                                Raising or, cheep in plows. In the straw rain barrels. Apples ducks straw, quail a ostriches donkey, hay hook cucumbers. Prairie dogs raccoons robins rats. Blue berries pigeons buzz and bean prairie dogs nails at est. Ewes mushrooms zucchini in forage Harvester at sheep with tractor. Forage Harvester, bean and Silage dump, cultivator brussel sprouts harrows, celery dread with kale augers harrows. bull bowels cat chicken cow, calf donkey duck. Veterinarian at Seeder eggs with watermelon ostriches. Forag.
+                            </p>
                         </div>
-                </div>
 
-                <div id="careers" className="info-div">
-                    <h2>
+                        <div className="pic right">
+                            <img loading="lazy" src={manager} width='100%' alt="dark brown curly haired woman snuggling a white dog" />
+                        </div>
+                    </div>
+                </CommonInfoSection>
+
+                <CommonInfoSection>
+                    <CommonH2>
+                        Meet the Team!
+                    </CommonH2>
+
+                    <div className="bio">
+                        <div className="pic left">
+                            <img loading="lazy" src={team} width='100%' alt="four dogs tugging at a toy" />
+                        </div>
+                        <div className="text right">
+                            <p>
+                                Veterinarian at Seeder eggs with watermelon ostriches. Pick up truck livestock, pets and storage shed, troughs feed bale manure, is garden wheat oats at augers. Killer scourge scared, drowning helpless sheep at, farmers market and cultivator ostrich. John Deere bees, parsley sweet corn at, porky pig shovels. Kidney beans ostrich trucks. Lamb in eggplant baler rain barrels manure hay rake. Killer scourge scared, drowning helpless sheep at.
+                            </p>
+                        </div>
+                    </div>
+                </CommonInfoSection>
+
+                <CommonInfoSection>
+                    <CommonH2>
                         Ready to Join the Team?
-                    </h2>
-
-                        <div className="p-div">
-                            <p>
-                                Paragraph about joining the team.
-                            </p>
-                            <br/>
-                            <p>
-                                Lamb in eggplant baler rain barrels manure hay rake. Gourds utters at welding equipment a oink oink haybine. Forage Harvester rakes peacocks, squeal garden woof. Haybine carrots soybeans, owls duck raising or, cheep in plows. Ewes fox, hay hook hay manure, John Deere radish barn, a hay loft house at pony. Haybine carrots soybeans, owls duc.
-                            </p>
-                        </div>
-                </div>
-            </div>
-        </div>
+                    </CommonH2>
+                    
+                    <div className="p-div">
+                        <p>
+                            Paragraph about joining the team.
+                        </p>
+                        <br/>
+                        <p>
+                            Lamb in eggplant baler rain barrels manure hay rake. Gourds utters at welding equipment a oink oink haybine. Forage Harvester rakes peacocks, squeal garden woof. Haybine carrots soybeans, owls duck raising or, cheep in plows. Ewes fox, hay hook hay manure, John Deere radish barn, a hay loft house at pony. Haybine carrots soybeans, owls duc.
+                        </p>
+                    </div>
+                </CommonInfoSection>
+            </CommonInfoSection>
+        </CommonStartDiv>
     )
 }
---
+++ src/styles/about.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const teamImg = styled.img`
+    width: 40%;
+`
