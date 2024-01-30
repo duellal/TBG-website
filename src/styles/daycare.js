985d08036cc9269ef3b34dd52f6d471485795da9
Alex <alexandriaduell@gmail.com>
2024-01-30 14:16:43 -0500

    fixed requirements + prices styles

--- src/styles/daycare.js
+++ src/styles/daycare.js
@@ -2,8 +2,12 @@ import styled from "styled-components";
 import { PricesTextSection } from "./commonBDG";
 
 
+export const DcPricesTextSection = styled(PricesTextSection)`
+height: 200px;
+`
+
 export const DcPackageDiv = styled.div`
-width: 300px;
+width: 100%;
 display: flex;
 flex-wrap: wrap;
 justify-content: center;
@@ -13,10 +17,5 @@ margin: 10px 0 15px 0;
 export const DcPackageP = styled.p`
 width: 100%;
 font-size: 2rem;
-// margin: 5px 0;
 text-align: center;
-`
-
-export const DcPricesTextSection = styled(PricesTextSection)`
-height: 200px;
 `
