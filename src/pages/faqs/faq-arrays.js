3776ecf5d0756aa99e4001820500c06d330adc3d
Alex <alexandriaduell@gmail.com>
2024-01-29 20:39:43 -0500

    daycare faqs inputted and styled

--- src/pages/faqs/faq-arrays.js
+++ src/pages/faqs/faq-arrays.js
@@ -8,61 +8,61 @@ let titles = [
 let generalFaqs = [
     {
         question: 'Question 1',
-        answer: 'Answer 1'
+        answer: ['Answer 1']
     },
     {
         question: `Question 2`,
-        answer: `Answer 2`
+        answer: [`Answer 2`]
     },
     {
         question: `Question 3`,
-        answer: `Answer 3`
-    }
+        answer: [`Answer 3`]
+    },
 ]
 
 let boardingFaqs = [
     {
         question: 'Question 1',
-        answer: 'Answer 1'
+        answer: ['Answer 1']
     },
     {
         question: `Question 2`,
-        answer: `Answer 2`
+        answer: [`Answer 2`]
     },
     {
         question: `Question 3`,
-        answer: `Answer 3`
-    }
+        answer: [`Answer 3`]
+    },
 ]
 
 let daycareFaqs = [
     {
-        question: 'Question 1',
-        answer: 'Answer 1'
+        question: 'What time is pick up and drop for daycare?',
+        answer: [`You may pick up and drop off anytime during our open hours. We are open from 7am- 7pm Monday - Saturday.`, `Half day daycare options are either for the morning play groups or the afternoonplay groups meaning that if you choose the half day morning play option, you can drop off and pick up anytime during 7am - 12pm. The half day afternoon play group drop off and pick up hours are anytime during 2pm - 7pm.`]
     },
     {
-        question: `Question 2`,
-        answer: `Answer 2`
+        question: `What is your Cancellation Policy for daycare?`,
+        answer: [`Cancellations or rescheduling for daycare must be made no later than 9 AM the morning of the reservation. Failure to cancel or reschedule a daycare reservation will result in a charge to the credit card on file for either a full day or a half day, depending on the reservation made. Clients with packages will forfeit 1 day of their package.`]
     },
     {
-        question: `Question 3`,
-        answer: `Answer 3`
-    }
+        question: `Can I add a Spa day to my pet’s Daycare day?`,
+        answer: [`You sure can! We can add a nail trim and/or a bath to your pet’s day. Please let us know at time of drop off what service you would like added and what time you would like to pick them up so we can maximize their play time and have them ready for pick up.`]
+    },
 ]
 
 let groomingFaqs = [
     {
         question: 'Question 1',
-        answer: 'Answer 1'
+        answer: ['Answer 1']
     },
     {
         question: `Question 2`,
-        answer: `Answer 2`
+        answer: [`Answer 2`]
     },
     {
         question: `Question 3`,
-        answer: `Answer 3`
-    }
+        answer: [`Answer 3`]
+    },
 ]
 
 export {
