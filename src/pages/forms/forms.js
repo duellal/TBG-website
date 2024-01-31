ec2f0bf608184d1d74b68d6629975051077f4bde
Alex <alexandriaduell@gmail.com>
2024-01-30 20:29:39 -0500

    commented out intake form + revised forms page

--- src/pages/forms/forms.js
+++ src/pages/forms/forms.js
@@ -1,7 +1,4 @@
 /* 
-    ALL pages have an expanded different photo - same like header below the nav bar
-        Make long text boxes shorter (less width)
-
     Put intake form where button to digital form goes
     Have a small box for intake form
         User puts in info + clicks next for the next section
@@ -27,7 +24,7 @@
 
 /* eslint-disable react/prop-types */
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 //Forms Styles:
 import '../../styles/forms.css'
@@ -36,30 +33,35 @@ import '../../styles/forms.css'
 import intakeForm from './waiver/TBG-Intake-Form-2024.pdf'
 
 export default function Forms(){
-    let navigate = useNavigate();
+    // let navigate = useNavigate();
 
     return (
         <div id="forms">
-            <h1> Forms  </h1>
+            {/* <h1> Forms  </h1> */}
+            <h2> Intake Form </h2>
+
 
             <div id="digital">
                 <div id="intake">
-                    <h2> Intake Form </h2>
 
                     <p> 
                         We require all owners to fill out an intake form to insure we have your contact information and all of your pets information. 
                     </p>
                     <br/>
                     <p>
-                        This form is required before you can schedule at our facility. 
+                        You can contact us at {' '}
+                            <a href="mailto:thebiscuitgarden@gmail.com">
+                                thebiscuitgarden@gmail.com
+                            </a>
+                        {' '}and we can email you a copy or you can come by and fill one out!
                     </p>
 
                     {/* Need button to navigate to Intake Form Page once in router */}
-                    <button className="intake" onClick={() => navigate('/forms/intake')}> Digital Intake Form </button>
+                    {/* <button className="intake" onClick={() => navigate('/forms/intake')}> Digital Intake Form </button> */}
                 </div>
             </div>
 
-            <div id="pdf">
+            {/* <div id="pdf">
                 <h2> Having Trouble Completing the Form Digitally? </h2>
                 <p>
                     If any of our forms do not work for you, please download a printable version by clicking the buttons below. You are more than welcome to complete them by hand and bring them in with you on your next visit, or you may email it to us at thebiscuitgarden.com.
@@ -68,7 +70,7 @@ export default function Forms(){
                 <div className="btns">
                     <button className="intake" onClick={() => window.open(intakeForm)}> Intake Form </button>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
