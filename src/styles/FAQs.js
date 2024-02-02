56994d33160ece699b79c217660ba3902f2d884b
Alex <alexandriaduell@gmail.com>
2024-02-01 22:43:06 -0500

    Added shadow behind FAQ boxes

--- src/styles/FAQs.js
+++ src/styles/FAQs.js
@@ -7,6 +7,8 @@ export const AllFaqs = styled.div`
     justify-content: center;
     margin-top: 30px;
     width: 80%;
+box-shadow: 0 0 20px 1px black;
+
 `
 
 export const FaqTitleDiv = styled.div`
