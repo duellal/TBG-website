54f3876c489abfb17cd06e256bb66d59ac690422
Alex <alexandriaduell@gmail.com>
2024-02-07 14:33:45 -0500

    Editted wording for asterisk requirement

--- src/pages/forms/components/asterisk-header.js
+++ src/pages/forms/components/asterisk-header.js
@@ -5,7 +5,7 @@ import { FormAsterisk, FormAsteriskH4 } from "../../../styles/forms";
 export default function AsteriskHeader(){
     return (
         <FormAsteriskH4> 
-            Required information will be starred red (
+            Required information will be starred in red (
                 <FormAsterisk> * </FormAsterisk>
                 )
         </FormAsteriskH4>
