e580808c7a57675b5b4a48b8c5e69b8183633ec8
Alex <alexandriaduell@gmail.com>
2024-02-27 14:11:07 -0500

    Added home folder in Pages folder, moved home.js + home-page-btns.js to it. Deleted constants folder

---
+++ src/pages/home/home.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+// Images:
+import { homeImages } from "../general-components/slideshow/banner-pics-arr";
+import collage from '../../images/home-collage.jpg'
+
+// Components:
+import Banner from '../general-components/slideshow/banner-slideshow'
+import InfoButton from "../general-components/info-buttons.js/info-button";
+
+// Styles:
+import { CommonStartDiv } from '../../styles/commonBDG'
+
+//Variables:
+import { home_btns } from "./home-page-btns";
+import { CollageImg } from "../../styles/common-styles";
+
+
+export default function Home(){
+    return (
+        <>
+            <Banner allImages={homeImages}/>
+            <CommonStartDiv>
+                <InfoButton array={home_btns}/>
+            </CommonStartDiv>
+
+            <CollageImg src={collage} alt=""/>
+        </>
+    )
+}
